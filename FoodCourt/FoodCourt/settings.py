29bc3e998f5135df2da75a0c4aafe268932cc4ce
KhyatiVerma-06 <khyativerma16@gmail.com>
2026-09-21 19:33:48 +0530

    Fix static files for Render deployment

--- FoodCourt/FoodCourt/settings.py
+++ FoodCourt/FoodCourt/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,6 +153,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
